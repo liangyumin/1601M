88565c04e74ab2a6e3f81eeaee757d66b5c93f67
crj <crj629@qq.com>
2018-09-03 13:33:34 +0800

    add 8.21_01.py

---
+++ 8.21_01.py
@@ -0,0 +1,20 @@
+# def add2(x=0,y=0):
+#     return [x+2,y+2]
+# print(add2())
+# def add3(x,y):
+#     return x+3,y+3
+# a,b=add3(1,2)
+# print(a,b)
+
+# a=[1,2,3]
+# b=[4,5,6]
+# c=map(lambda x,y:x*y,a,b)
+# c=list(c)
+# print(c)
+
+# from functools import reduce
+# n=1
+# reduce(lambda x,y:x*y,range(1,n+1))
+# b=filter(lambda x:x>5 and x<8,range(10))
+# b=list(b)
+# print(b)
